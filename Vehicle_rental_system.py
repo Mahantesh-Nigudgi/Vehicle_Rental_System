963c82b68586869f2746665f3f7b21f4cd1b5549
Mahantesh S Nigudgi <mantunigudgi@gmail.com>
2026-08-16 00:44:44 +0530

    Add files via upload

--- Vehicle_rental_system.py
+++ Vehicle_rental_system.py
@@ -2,20 +2,24 @@ class Vehicle_rental:
 
     def __init__(self):
         self.vehicles=[
-            {'Vehicle_type' : 'Car','Vehicle_number' : 'KA01RL1234','Rent' : 1600,'Vehicel_company' :'TATA','Vechile_model' : 'Nexon','Engine_capcity' : '1200cc','Seating_capacity':5},
-            {'Vehicle_type' : 'Car','Vehicle_number' : 'KA01MA2233','Rent' : 2000,'Vehicel_company' :'TOYOTA','Vechile_model' : 'Innova','Engine_capcity' : '1600cc','Seating_capacity':7},
-            {'Vehicle_type' : 'Car','Vehicle_number' : 'KA07DG8121','Rent' : 800,'Vehicel_company' :'NISSAN','Vechile_model' : 'Micra','Engine_capcity' : '800cc','Seating_capacity':5},
-            {'Vehicle_type' : 'Car','Vehicle_number' : 'KA01DQ3421','Rent' : 1200,'Vehicel_company' :'MARUTI','Vechile_model' : 'Eratiga','Engine_capcity' : '950cc','Seating_capacity':7},
-            {'Vehicle_type' : 'Bike','Vehicle_number' : 'KA01DF2893','Rent' : 400,'Vehicel_company' :'HONDA','Vechile_model' : 'Activa','Engine_capcity' : '125cc','Seating_capacity':2},
-            {'Vehicle_type' : 'Bike','Vehicle_number' : 'KA05PD3376','Rent' : 750,'Vehicel_company' :'RE','Vechile_model' : 'Bullet350','Engine_capcity' : '349cc','Seating_capacity':2},
-            {'Vehicle_type' : 'Bike','Vehicle_number' : 'KA05EJ7854','Rent' : 450,'Vehicel_company' :'TVS','Vechile_model' : 'Raider','Engine_capcity' : '200cc','Seating_capacity':2},
-            {'Vehicle_type' : 'Bike','Vehicle_number' : 'KA07DG5321','Rent' : 550,'Vehicel_company' :'BAJA','Vechile_model' : 'NS200','Engine_capcity' : '200cc','Seating_capacity':2},
+            {'Vehicle_type' : 'Car','Vehicle_number' : 'KA01RL1234','Rent' : 1600,'Vehicle_company' :'TATA','Vehicle_model' : 'Nexon','Engine_capacity' : '1200cc','Seating_capacity':5},
+            {'Vehicle_type' : 'Car','Vehicle_number' : 'KA01MA2233','Rent' : 2000,'Vehicle_company' :'TOYOTA','Vehicle_model' : 'Innova','Engine_capacity' : '1600cc','Seating_capacity':7},
+            {'Vehicle_type' : 'Car','Vehicle_number' : 'KA07DG8121','Rent' : 800,'Vehicle_company' :'NISSAN','Vehicle_model' : 'Micra','Engine_capacity' : '800cc','Seating_capacity':5},
+            {'Vehicle_type' : 'Car','Vehicle_number' : 'KA01DQ3421','Rent' : 1200,'Vehicle_company' :'MARUTI','Vehicle_model' : 'Eratiga','Engine_capacity' : '950cc','Seating_capacity':7},
+            {'Vehicle_type' : 'Bike','Vehicle_number' : 'KA01DF2893','Rent' : 400,'Vehicle_company' :'HONDA','Vehicle_model' : 'Activa','Engine_capacity' : '125cc','Seating_capacity':2},
+            {'Vehicle_type' : 'Bike','Vehicle_number' : 'KA05PD3376','Rent' : 750,'Vehicle_company' :'RE','Vehicle_model' : 'Bullet350','Engine_capacity' : '349cc','Seating_capacity':2},
+            {'Vehicle_type' : 'Bike','Vehicle_number' : 'KA05EJ7854','Rent' : 450,'Vehicle_company' :'TVS','Vehicle_model' : 'Raider','Engine_capacity' : '200cc','Seating_capacity':2},
+            {'Vehicle_type' : 'Bike','Vehicle_number' : 'KA07DG5321','Rent' : 550,'Vehicle_company' :'BAJA','Vehicle_model' : 'NS200','Engine_capacity' : '200cc','Seating_capacity':2},
             ]
             
 
     @staticmethod
     def greet():
         print("\n!!.....Welcome to Nama Vehicle Rentals.....!!\n")
+
+    @staticmethod
+    def greet_1():
+        print('!!.....Thank you please visit again.....!!')
 
 
     def vehicle_details(self):
@@ -27,29 +31,30 @@ class Vehicle_rental:
          
     def car_details(self):
         for val in self.vehicles:
-            # print(val)
             if val['Vehicle_type']=='Car':
                 for k,v in val.items():
                     print(k,':',v)
                 print()
 
     def car_total_rent(self,model,days):
+        car=False
         for val in self.vehicles:
-            if val['Vechile_model']==model:
-                if val['Vechile_model']==model:
-                    print("----------TOTAL RENT----------")
-                    print()
-                    print('Vehicle :',val['Vechile_model'])
-                    print('Rent per day :',val['Rent'])
-                    print('Number of days :',days)
-                    print()
-                    print('------------------------------')
-                    print()
-                    print('The Total Rent :', val['Rent'],'X', days, '=' ,val['Rent']*days)
-                else:
-                    print()
-                    print('This car is not available, so please check for other car')
-                    break
+            if val['Vehicle_model']==model:
+                car=True
+                print("----------TOTAL RENT----------")
+                print()
+                print('Vehicle :',val['Vehicle_model'])
+                print('Rent per day :',val['Rent'])
+                print('Number of days :',days)
+                print()
+                print('------------------------------')
+                print()
+                print('The Total Rent :', val['Rent'],'X', days, '=' ,val['Rent']*days)
+                break
+        if not car: 
+            print()
+            print('This car is not available, so please check for other car')
+            
 
             
 
@@ -62,29 +67,41 @@ class Vehicle_rental:
 
 
     def bike_total_rent(self,model,days):
+            bike=False
             for val in self.vehicles:
-                if val['Vechile_model']==model:
-                    if val['Vechile_model']==model:
-                        print("----------TOTAL RENT----------")
-                        print()
-                        print('Vehicle :',val['Vechile_model'])
-                        print('Rent per day :',val['Rent'])
-                        print('Number of days :',days)
-                        print()
-                        print('------------------------------')
-                        print()
-                        print('The Total Rent :', val['Rent'],'X', days, '=' ,val['Rent']*days)
-                    else:
-                        print()
-                        print('This bike is not available, so please check for other bike')
-                        break
+                if val['Vehicle_model']==model :
+                    bike=True
+                    print("----------TOTAL RENT----------")
+                    print()
+                    print('Vehicle :',val['Vehicle_model'])
+                    print('Rent per day :',val['Rent'])
+                    print('Number of days :',days)
+                    print()
+                    print('------------------------------')
+                    print()
+                    print('The Total Rent :', val['Rent'],'X', days, '=' ,val['Rent']*days)
+                    break
+            if not bike:
+                print()
+                print('This bike is not available, so please check for other bike')
+                
 
 v1=Vehicle_rental()
 
 v1.greet()
 
 while True:
-    choice=print("Our Services :\n1.Display All The Vehicles .\n2.Display All The Cars. \n3.Display all the Bikes. \n4.Calcuate the rent of a car.  \n5.Calcuate the rent of a bike. \n6.Exit")
+    choice=print(
+        "Our Services : \n"
+        "1.Display All The Vehicles. \n"
+        "2.Display All The Cars. \n"
+        "3.Display all the Bikes. \n"
+        "4.Calcuate the rent of a car.  \n"
+        "5.Calcuate the rent of a bike. "
+        "\n6.Exit"
+        )
+
+    
     opt=int(input("Enter your choice :"))
     print()
 
@@ -114,7 +131,7 @@ while True:
             print()
 
         case 6:
-            print('!!.....Thank you please visit again.....!!')
+            v1.greet_1()
             break
 
         case _:
